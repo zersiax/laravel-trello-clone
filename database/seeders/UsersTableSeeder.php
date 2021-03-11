9e435656fb92ecc635f645e081e2281498d1ac92
Florian Beijers <florianbeijers@gmail.com>
2021-03-11 21:40:37 +0100

    Added db seeders

---
+++ database/seeders/UsersTableSeeder.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Database\Seeders;
+use App\Models\User;
+use Illuminate\Database\Seeder;
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        User::create([
+            'name' => 'John Doe',
+            'email' => 'demo@demo.com',
+            'password' => bcrypt('secret'),
+        ]);
+    }
+}
+
